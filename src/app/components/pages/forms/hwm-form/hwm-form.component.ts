a83239d6d901c3a6a85e80c2bfd62171f3a0fbc6
jpjangid <developerjp2000@gmail.com>
2022-09-02 11:42:43 +0530

    created hwm membership form

--- src/app/components/pages/forms/hwm-form/hwm-form.component.ts
+++ src/app/components/pages/forms/hwm-form/hwm-form.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import * as moment from 'moment';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ApiService } from 'src/app/services/api.service';
 
@@ -13,6 +14,7 @@ import { ApiService } from 'src/app/services/api.service';
 export class HWMFormComponent implements OnInit {
 
   hwmMemberShipFormData:any={product_detail:[{}],waste_detail:[{}]};
+  hwmDocuments:any=[]
   productDetails:any=[];
   categoryData:any=[];
   member_classification:any=[]
@@ -23,14 +25,28 @@ export class HWMFormComponent implements OnInit {
   //to register HWM membership candidate
   Register(form:NgForm) {
     console.log(form.value)
-    // if(form.valid) {
+    let formData = new FormData()
+
+    let dob1filterDate = moment(this.hwmMemberShipFormData.date_of_birth1).format('YYYY/MM/DD');
+    this.hwmMemberShipFormData.date_of_birth1 = dob1filterDate;
+
+    let dob2filterDate = moment(this.hwmMemberShipFormData.date_of_birth2).format('YYYY/MM/DD');
+    this.hwmMemberShipFormData.date_of_birth2 = dob2filterDate;
+
+    let wasteDisposalDate = moment(this.hwmMemberShipFormData.hw_disposal_expected_date).format('YYYY/MM/DD');
+    this.hwmMemberShipFormData.hw_disposal_expected_date = wasteDisposalDate;
+
+    let production_commencement_date = moment(this.hwmMemberShipFormData.production_commencement_date).format('YYYY/MM/DD');
+    this.hwmMemberShipFormData.production_commencement_date = production_commencement_date;
+
+    if(this.hwmMemberShipFormData.valid) {
       this.apiservice.hwmRegistration(form.value).subscribe((res:any)=> {
         console.log(res);
       },
       (error:HttpErrorResponse)=> {
         console.log(error);
       })
-    // }
+    }
   }
 
   //to remove one row to product detail
@@ -55,5 +71,11 @@ export class HWMFormComponent implements OnInit {
     else {
       this.hwmMemberShipFormData.waste_detail.push({});
     }
+  }
+
+  //upload files as binary
+  onFileChange(event:any){
+    // this.files = event.target.files;
+    console.log(event,event.target.files);
   }
 }
